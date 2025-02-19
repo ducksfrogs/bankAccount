5a6334b6c95023cc34f8b963cbc353baafba822f
ducksfrogs <ma_yamaki@yahoo.com>
2025-02-20 06:09:23 +0900

    include some

---
+++ src/BankAccount.java
@@ -0,0 +1,59 @@
+public class BankAccount {
+    private int accountNumber = 1234567;
+    private double balance = 0.0;
+    private String accountType = "Nomal";
+    private String customerName = "John Doe";
+
+    public void setAccountNumber(int accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public void setAccountType(String accountType) {
+        this.accountType = accountType;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public void setCustomerEmail(String customerEmail) {
+        this.customerEmail = customerEmail;
+    }
+
+    public void setCustomerPhone(String customerPhone) {
+        this.customerPhone = customerPhone;
+    }
+
+    private String customerEmail = "john.doe@gmail.com";
+    private String customerPhone = "123456789";
+
+    public int getAccountNumber() {
+        return accountNumber;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public String getAccountType() {
+        return accountType;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public String getCustomerEmail() {
+        return customerEmail;
+    }
+
+    public String getCustomerPhone() {
+        return customerPhone;
+    }
+
+
+}
